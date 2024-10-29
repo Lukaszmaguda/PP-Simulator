4ce803f9678190f1b3502a91dee5ff5f880b4433
Lukaszmaguda <rometzet@gmail.com>
2024-10-29 08:15:03 +0100

    "Stwory"

---
+++ Simulator/Animals.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+    public string Info
+    {
+        get { return $"{Description}<{Size}>"; }
+    }
+}
