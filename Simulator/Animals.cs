e40314cb347dd94ca603f4ff1fa58e70b5bf3d40
Lukaszmaguda <rometzet@gmail.com>
2024-10-29 14:15:19 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,13 +1,34 @@
-﻿using System.Reflection.Emit;
+﻿
 using System.Xml.Linq;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public string Description
+    {
+        get => description;
+        init
+        {
+            description = value.Trim();
+            if (description.Length < 3 ) description = description.PadRight(3, '#');
+            if (description.Length > 15)
+            {
+                description = description.Substring(0, 15).TrimEnd();
+                if (description.Length < 3)
+                {
+                    description = description.PadRight(3, '#');
+                }
+            }
+
+            description = char.ToUpper(description[0]) + description.Substring(1);
+        }
+    }
     public uint Size { get; set; } = 3;
 
     public string Info
     {
         get { return $"{Description}<{Size}>"; }
     }
+
+
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,21 +1,56 @@
-﻿using System.Reflection.Emit;
-using System.Xml.Linq;
-
+﻿
 namespace Simulator
 {
     internal class Creature
     {
-        public string Name { get; set; }
-        public int Level { get; set; }
+        private string name = "Unknown";
+        private int level = 1;
+
+        public String Name
+        {
+            get => name;
+            init  
+            {
+                name = value.Trim();
+                if (name.Length < 3) name = name.PadRight(3, '#');                
+                if (name.Length > 25) 
+                {
+                    name = name.Substring(0, 25).TrimEnd();
+                    if (name.Length < 3)
+                    {
+                        name = name.PadRight(3, '#');
+                    }
+                }
+                name = char.ToUpper(name[0]) + name.Substring(1);
+
+            }
+        }
+        public int Level
+        {
+            get => level;
+            init
+            {
+                if (value < 1) level = 1;
+                else if (value > 10) level = 10;
+                else level = value;
+            }
+        }
+        public void Upgrade()
+        {
+            if (level < 10) level++;
+        }
+
 
         public Creature(string name, int level = 1)
         {
             Name = name;
             Level = level;
         }
+
+
         public Creature() { }
 
-        public void sayHi()
+        public void SayHi()
         {
             Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
         }
