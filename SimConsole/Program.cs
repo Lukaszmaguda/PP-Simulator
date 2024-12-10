36a5bf0e0d95cdd21fae41c79ab4fcd6f396da40
Lukaszmaguda <rometzet@gmail.com>
2024-12-10 01:52:42 +0100

    Moving animals

--- SimConsole/Program.cs
+++ SimConsole/Program.cs
@@ -11,53 +11,45 @@ internal class Program
         Console.WriteLine("Starting Simulator!\n");
         Console.OutputEncoding = Encoding.UTF8;
 
-        // Inicjalizacja mapy torusowej
         SmallTorusMap map = new(8, 6);
 
-        // Tworzenie stworów i zwierząt
         var mappables = new List<IMappable>
         {
             new Elf("Elandor"),
             new Orc("Gorbag"),
-            new Animals("Rabbits", 5), // Stado królików
-            new Birds("Eagles", 4, true), // Grupa orłów (latające)
-            new Birds("Ostriches", 3, false) // Grupa strusi (nieloty)
+            new Animals("Rabbits", 5), 
+            new Birds("Eagles", 4, true), 
+            new Birds("Ostriches", 3, false) 
         };
 
-        // Pozycje startowe stworów i zwierząt
         var points = new List<Point>
         {
-            new Point(1, 1), // Pozycja elfa
-            new Point(3, 3), // Pozycja orka
-            new Point(0, 4), // Pozycja królików
-            new Point(6, 0), // Pozycja orłów
-            new Point(2, 2)  // Pozycja strusi
+            new Point(1, 1), 
+            new Point(3, 3), 
+            new Point(0, 4), 
+            new Point(6, 0), 
+            new Point(2, 2)  
         };
 
-        // Łańcuch ruchów
-        string moves = "dlruudldrulldr"; // 15 ruchów
+        string moves = "dlruudldrulldr";
 
-        // Tworzenie symulacji
         Simulation simulation = new(map, mappables, points, moves);
 
-        // Wizualizacja mapy
         MapVisualizer mapVisualizer = new(simulation.Map);
 
         Console.WriteLine("Press Enter to start simulation...");
         Console.ReadLine();
 
-        // Pętla symulacji
         while (!simulation.Finished)
         {
-            mapVisualizer.Draw(); // Rysowanie mapy
+            mapVisualizer.Draw(); 
 
             Console.WriteLine("\nNaciśnij dowolny klawisz, aby wykonać kolejny ruch...");
             Console.ReadKey(true);
 
-            simulation.Turn(); // Kolejny ruch symulacji
+            simulation.Turn(); 
         }
 
-        // Koniec symulacji
         Console.WriteLine("\nSymulacja zakończona.");
     }
 }
