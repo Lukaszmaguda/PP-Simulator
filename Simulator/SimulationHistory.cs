b0da670f2a8df51bf4d4d66d301bc60a9bc3b479
Lukaszmaguda <rometzet@gmail.com>
2025-01-02 19:11:36 +0100

    simweb v2

--- Simulator/SimulationHistory.cs
+++ Simulator/SimulationHistory.cs
@@ -1,45 +1,55 @@
-﻿using Simulator;
-namespace Simulator;
-public class SimulationHistory
+﻿namespace Simulator
 {
-    private Simulation _simulation { get; }
-    public int SizeX { get; }
-    public int SizeY { get; }
-    public List<SimulationTurnLog> TurnLogs { get; } = [];
-    // store starting positions at index 0
-
-    public SimulationHistory(Simulation simulation)
+    public class SimulationHistory
     {
-        _simulation = simulation ??
-            throw new ArgumentNullException(nameof(simulation));
-        SizeX = _simulation.Map.SizeX;
-        SizeY = _simulation.Map.SizeY;
-        Run();
-    }
+        private readonly Simulation _simulation;
 
-    private void Run()
-    {
-        while (!_simulation.Finished)
+        public int SizeX { get; }
+        public int SizeY { get; }
+        public List<SimulationTurnLog> TurnLogs { get; } = new();
+
+        public SimulationHistory(Simulation simulation)
         {
-            _simulation.Turn();
+            _simulation = simulation ?? throw new ArgumentNullException(nameof(simulation));
+            SizeX = _simulation.Map.SizeX;
+            SizeY = _simulation.Map.SizeY;
+            Run();
+        }
 
-            var symbols = new Dictionary <Point, List<char>>();
-            foreach (var mappable in _simulation.IMappables)
+        private void Run()
+        {
+            while (!_simulation.Finished)
             {
-                if (!symbols.ContainsKey(mappable.Position))
+                _simulation.Turn();
+                var symbols = new Dictionary<Point, List<char>>();
+
+                foreach (var mappable in _simulation.IMappables)
                 {
-                    symbols[mappable.Position] = new List<char>();
+                    if (!symbols.ContainsKey(mappable.Position))
+                    {
+                        symbols[mappable.Position] = new List<char>();
+                    }
+                    symbols[mappable.Position].Add(mappable.Symbol);
                 }
-                symbols[mappable.Position].Add(mappable.Symbol);
-            }
 
-            var log = new SimulationTurnLog
-            {
-                Mappable = _simulation.CurrentIMappable.ToString(),
-                Move = _simulation.CurrentMoveName,
-                Symbols = symbols
-            };
-            TurnLogs.Add(log);
+                var log = new SimulationTurnLog
+                {
+                    Mappable = _simulation.CurrentIMappable.ToString(),
+                    Move = _simulation.CurrentMoveName,
+                    Symbols = symbols
+                };
+                TurnLogs.Add(log);
+            }
+        }
+
+        public IEnumerable<SimulationState> GetStates()
+        {
+            return TurnLogs.Select(log => new SimulationState(
+                log.Symbols.Keys.ToList(),
+                log.Symbols.Values.SelectMany(s => s).ToList(),
+                log.Mappable.FirstOrDefault(),
+                log.Move
+            ));
         }
     }
 }
